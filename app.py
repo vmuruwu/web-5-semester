2f8c66c231f3b452b2f64e59b651c658205c2245
Наталья Втюрина <vmuruwu@yandex.ru>
2024-09-28 01:05:35 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ def not_found(err):
     return "нет такой страницы", 404
 
 @app.route("/")
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return """<!doctype html>
         <html>
            <body>
                <h1>web-сервер на flask</h1>
-               <a href="/author">author</a>
+               <a href="/lab1/author">author</a>
            </body>
         </html>""", 200, {
             'X-Server': 'sample',
             'Content-Type': 'text/plain; charset=utf-8'
             }
 
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = "Втюрина Наталья Артёмовна"
     group = "ФБИ-24"
@@ -31,7 +31,7 @@ def author():
                 <p>Студент: """ + name + """</p>
                 <p>Группа: """ + group + """</p>
                 <p>Факультет: """ + faculty + """</p>
-                <a href="/web">web</a>
+                <a href="/lab1/web">web</a>
             </body>
         </html>"""
 
@@ -73,7 +73,7 @@ def reset_counter():
     count = 0  
     return redirect(url_for('counter'))
 
-@app.route("/info")
+@app.route("/lab1/info")
 def info():
     return redirect("/author")
 
