d10884d656b9bdf8f78d7d1f7ef7538d5062f800
Наталья Втюрина <vmuruwu@yandex.ru>
2024-10-25 23:43:13 +0700

    Цикл в шаблоне

--- app.py
+++ app.py
@@ -286,9 +286,14 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    name = 'Втюрина Наталья'
-    lab_num = '2'
-    year_num = '3 курс'
-    group = 'ФБИ-24'
-    return render_template('example.html', name = name,
-    lab_num = lab_num, year_num = year_num, group = group)
+    name, lab_num, year_num, group = 'Втюрина Наталья', '2', '3 курс', 'ФБИ-24'
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+    ]
+    return render_template('example.html',
+                            name = name, lab_num = lab_num, year_num = year_num,
+                            group = group, fruits = fruits)
