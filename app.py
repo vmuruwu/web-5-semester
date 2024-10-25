cdcba20cfee912cb94fe26f432640b6e916fb65b
Наталья Втюрина <vmuruwu@yandex.ru>
2024-10-26 00:07:55 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -297,3 +297,7 @@ def example():
     return render_template('example.html',
                             name = name, lab_num = lab_num, year_num = year_num,
                             group = group, fruits = fruits)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
