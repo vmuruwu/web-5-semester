3e0688e36dcf3b48e75983194899d5f0390057ae
Наталья Втюрина <vmuruwu@yandex.ru>
2024-10-27 19:14:03 +0700

    Выражения

--- app.py
+++ app.py
@@ -346,3 +346,15 @@ def lab2():
 def filters():
     phrase = "О <b>сколько</b> <u>нам</u> <i>открытий</i> чудных..."
     return render_template('filter.html', phrase = phrase)
+
+@app.route('/lab2/calc/<int:a>/<int:b>')
+def calc(a, b):
+    return render_template('calc.html', a = a, b = b)
+
+@app.route('/lab2/calc/')
+def default_calc():
+    return redirect('/lab2/calc/1/1')
+
+@app.route('/lab2/calc/<int:a>')
+def single_param_calc(a):
+    return redirect(f'/lab2/calc/{a}/1')
