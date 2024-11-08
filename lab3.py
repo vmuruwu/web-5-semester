009328d77a73aa96247f230250388bae8e2f3eca
Наталья Втюрина <vmuruwu@yandex.ru>
2024-11-09 01:20:12 +0700

    Поиск товаров

--- lab3.py
+++ lab3.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for, redirect, render_template, request, make_response
+from flask import Blueprint, url_for, redirect, render_template, request, make_response, abort
 lab3 = Blueprint('lab3', __name__)
 
 @lab3.route('/lab3/')
@@ -151,3 +151,39 @@ def process_ticket():
                            age=age, departure=departure, destination=destination, date=date, 
                            insurance=insurance, ticket_type=ticket_type, price=price)
 
+
+products = [
+    {"name": "Смартфон A", "price": 15000, "brand": "Samsung", "color": "Черный"},
+    {"name": "Смартфон B", "price": 25000, "brand": "LG", "color": "Белый"},
+    {"name": "Ноутбук A", "price": 55000, "brand": "Samsung", "color": "Серый"},
+    {"name": "Ноутбук B", "price": 75000, "brand": "LG", "color": "Синий"},
+    {"name": "Планшет A", "price": 20000, "brand": "Samsung", "color": "Красный"},
+    {"name": "Планшет B", "price": 30000, "brand": "LG", "color": "Зеленый"},
+    {"name": "Телевизор A", "price": 45000, "brand": "Samsung", "color": "Черный"},
+    {"name": "Телевизор B", "price": 65000, "brand": "LG", "color": "Белый"},
+    {"name": "Часы A", "price": 5000, "brand": "Samsung", "color": "Серебристый"},
+    {"name": "Часы B", "price": 10000, "brand": "LG", "color": "Золотистый"},
+    {"name": "Камера A", "price": 30000, "brand": "Samsung", "color": "Черный"},
+    {"name": "Камера B", "price": 45000, "brand": "LG", "color": "Белый"},
+    {"name": "Наушники A", "price": 3000, "brand": "Samsung", "color": "Синий"},
+    {"name": "Наушники B", "price": 7000, "brand": "LG", "color": "Зеленый"},
+    {"name": "Колонка A", "price": 8000, "brand": "Samsung", "color": "Красный"},
+    {"name": "Колонка B", "price": 12000, "brand": "LG", "color": "Черный"},
+    {"name": "Монитор A", "price": 20000, "brand": "Samsung", "color": "Серый"},
+    {"name": "Монитор B", "price": 35000, "brand": "LG", "color": "Белый"},
+    {"name": "Клавиатура A", "price": 2500, "brand": "Samsung", "color": "Черный"},
+    {"name": "Клавиатура B", "price": 4500, "brand": "LG", "color": "Белый"}
+]
+
+
+@lab3.route('/lab3/product_search', methods=['GET', 'POST'])
+def product_search():
+    min_price = request.args.get('min_price', type=int)
+    max_price = request.args.get('max_price', type=int)
+    
+    if min_price is None or max_price is None:
+        return render_template('lab3/product_search.html')
+    
+    filtered_products = [product for product in products if min_price <= product["price"] <= max_price]
+    
+    return render_template('lab3/product_results.html', products=filtered_products, min_price=min_price, max_price=max_price)
