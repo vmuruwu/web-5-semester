cd832c4112d3466dd825fbe88ee78b061c687769
Наталья Втюрина <vmuruwu@yandex.ru>
2024-11-18 22:19:58 +0700

    Валидация

--- lab4.py
+++ lab4.py
@@ -100,14 +100,14 @@ def mult():
 
 tree_count = 0
 
-@lab4.route('/lab4/tree', methods = ['GET', 'POST'])
+@lab4.route('/lab4/tree', methods=['GET', 'POST'])
 def tree():
     global tree_count
     if request.method == 'GET':
-        return render_template('lab4/tree.html', tree_count = tree_count)
+        return render_template('lab4/tree.html', tree_count=tree_count)
     operation = request.form.get('operation')
-    if operation == 'cut':
+    if operation == 'cut' and tree_count > 0:
         tree_count -= 1
-    elif operation == 'plant':
+    elif operation == 'plant' and tree_count < 10:
         tree_count += 1
     return redirect('/lab4/tree')
