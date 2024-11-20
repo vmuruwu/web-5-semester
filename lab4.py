0cf64b265054d6ef3853282898362a52a925113a
Наталья Втюрина <vmuruwu@yandex.ru>
2024-11-20 23:06:13 +0700

    Список пользователей

--- lab4.py
+++ lab4.py
@@ -113,6 +113,13 @@ def tree():
     return redirect('/lab4/tree')
 
 
+users = [
+    {'login': 'alex', 'password': '123'},
+    {'login': 'bob', 'password': '555'},
+    {'login': 'sara', 'password': '789'},
+    {'login': 'marisha', 'password': '222'}
+]
+
 @lab4.route('/lab4/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -120,8 +127,9 @@ def login():
     login = request.form.get('login')
     password = request.form.get('password')
 
-    if login == 'alex' and password == '123':
-        return render_template('lab4/login.html', login = login, authorized = True)
+    for user in users:
+        if login == user['login'] and password == user['password']:
+            return render_template('lab4/login.html', login = login, authorized = True)
     
     error = 'Неверные логин и/или пароль'
     return render_template('lab4/login.html', error = error, authorized = False)
