c861fbeea3be7d4fd6f1c41565022c37e537d759
Наталья Втюрина <vmuruwu@yandex.ru>
2024-11-22 21:29:56 +0700

    Сессия

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from lab3 import lab3
 from lab4 import lab4
 
 app = Flask(__name__)
+
+app.secret_key = 'секретно-секретный секрет'
+
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
