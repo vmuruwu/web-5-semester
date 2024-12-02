fe773f09c672926825c92979cfc329df27e81d01
Наталья Втюрина <vmuruwu@yandex.ru>
2024-12-03 00:49:26 +0700

    Авторизация

--- lab5.py
+++ lab5.py
@@ -1,15 +1,53 @@
 from flask import Blueprint, render_template, request, redirect, session
 import psycopg2
+from psycopg2.extras import RealDictCursor
 
 lab5 = Blueprint('lab5', __name__)
 
 @lab5.route('/lab5/')
 def lab():
-    return render_template('lab5/lab5.html')
+    return render_template('lab5/lab5.html', login=session.get('login'))
 
-@lab5.route('/lab5/login')
+
+@lab5.route('/lab5/login', methods = ['GET', 'POST'])
 def login():
-    return "Войти"
+    if request.method == 'GET':
+        return render_template('lab5/login.html')
+
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login or password):
+        return render_template('lab5/login.html', error="Заполните поля")
+    
+    conn = psycopg2.connect(
+        host = '127.0.0.1',
+        database = 'natalya_vtyurina_knowledge_base',
+        user = 'natalya_vtyurina_knowledge_base',
+        password = '123'
+    )
+    cur = conn.cursor(cursor_factory = RealDictCursor)
+
+    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    user = cur.fetchone()
+
+    if not user:
+        cur.close()
+        conn.close()
+        return render_template ('lab5/login.html',
+                                    error = 'Логин и/или пароль неверны')
+
+    if user['password'] != password:
+        cur.close()
+        conn.close()
+        return render_template ('lab5/login.html',
+                                    error = 'Пароль введён неверно')
+    
+    session['login'] = login
+    cur.close()
+    conn.close()
+    return render_template('lab5/success_login.html', login=login)
+
 
 @lab5.route('/lab5/register', methods = ['GET', 'POST'])
 def register():
@@ -42,9 +80,11 @@ def register():
     conn.close()
     return render_template('lab5/success.html', login=login)
 
+
 @lab5.route('/lab5/list')
 def list_articles():
     return "Список статей"
+
 
 @lab5.route('/lab5/create')
 def create_article():
