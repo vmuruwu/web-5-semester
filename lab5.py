7288f02f65dfe38bb0056c9d46303b5a2d8ecef4
Наталья Втюрина <vmuruwu@yandex.ru>
2024-12-05 00:13:05 +0700

    Вывод статей

--- lab5.py
+++ lab5.py
@@ -86,7 +86,20 @@ def register():
 
 @lab5.route('/lab5/list')
 def list_articles():
-    return "Список статей"
+    login = session.get('login')
+    if not login:
+        return redirect('/lab5/login')
+    
+    conn, cur = db_connect()
+
+    cur.execute(f"SELECT id FROM users WHERE login='{login}';")
+    user_id = cur.fetchone()['id']
+
+    cur.execute(f"SELECT * FROM articles WHERE user_id='{user_id}';")
+    articles = cur.fetchall()
+
+    db_close(conn, cur)
+    return render_template('/lab5/articles.html', articles = articles)
 
 
 @lab5.route('/lab5/create', methods=['GET', 'POST'])
