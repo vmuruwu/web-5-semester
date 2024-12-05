2a1a8aba080222cb8947afa894ee486d953e48be
Наталья Втюрина <vmuruwu@yandex.ru>
2024-12-05 11:02:53 +0700

    импорт os

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 
+import os
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
