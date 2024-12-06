5cbc50ee6fb31d92cb1d8fff87d79366c1219571
Наталья Втюрина <vmuruwu@yandex.ru>
2024-12-07 01:48:40 +0700

    Бронирование офисов

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 
 import os
 
@@ -18,6 +19,7 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
 
 
 @app.errorhandler(404)
@@ -108,6 +110,7 @@ def index():
                     <li><a href="''' + url_for('lab3.lab') + '''">Третья лабораторная</a></li>
                     <li><a href="''' + url_for('lab4.lab') + '''">Четвёртая лабораторная</a></li>
                     <li><a href="''' + url_for('lab5.lab') + '''">Пятая лабораторная</a></li>
+                    <li><a href="''' + url_for('lab6.lab') + '''">Шестая лабораторная</a></li>
                 </ul>
             </div>
         </main>
