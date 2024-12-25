56873dac299593d3b4d6b9fc7e46d0e4ceb616eb
Наталья Втюрина <vmuruwu@yandex.ru>
2024-12-26 01:09:43 +0700

    Редактирование выбранного фильма по REST

--- lab7.py
+++ lab7.py
@@ -57,4 +57,11 @@ def del_film(id):
     del films[id]
     return '', 204
 
-# fetch ('/lab7/rest-api/films/0', {method: 'DELETE'})
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
+def put_film(id):
+    if id < 0 or id >= len(films):
+        abort(404)
+    film = request.get_json()
+    films[id] = film
+    return films[id]
+
