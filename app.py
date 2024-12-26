f1bf2b2d7ed03e13ca10fd6e5f827da7d653b386
Наталья Втюрина <vmuruwu@yandex.ru>
2024-12-27 00:29:38 +0700

    Заготовка к 8-ой лабораторной

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 
 import os
 
@@ -22,6 +23,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 
 
 @app.errorhandler(404)
@@ -114,6 +116,7 @@ def index():
                     <li><a href="''' + url_for('lab5.lab') + '''">Пятая лабораторная</a></li>
                     <li><a href="''' + url_for('lab6.lab') + '''">Шестая лабораторная</a></li>
                     <li><a href="''' + url_for('lab7.lab') + '''">Седьмая лабораторная</a></li>
+                    <li><a href="''' + url_for('lab8.lab') + '''">Восьмая лабораторная</a></li>
                 </ul>
             </div>
         </main>
