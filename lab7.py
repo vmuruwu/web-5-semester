4dcf3908c1cc52911ebfb81d0df530ff971e7f10
Наталья Втюрина <vmuruwu@yandex.ru>
2024-12-26 16:35:08 +0700

    Добавление нового фильма по REST

--- lab7.py
+++ lab7.py
@@ -65,3 +65,8 @@ def put_film(id):
     films[id] = film
     return films[id]
 
+@lab7.route('/lab7/rest-api/films/', methods=['POST'])
+def add_film():
+    film = request.get_json()
+    films.append(film)
+    return {'id': len(films) - 1}, 201
